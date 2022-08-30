cb8c2957eb44a30ccfda6f14cb77cfe1da15c71b
Rayyan <rayyanzaid0401@gmail.com>
2022-08-29 23:46:12 -0700

    created JWT token

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,4 +1,13 @@
 const Student = require('../models/StudentModel');
+const jwt = require('jsonwebtoken');
+
+
+// re-use during login and signup
+const createToken = (_id) => {
+    return jwt.sign({_id},process.env.JWT_SECRET_STRING, {expiresIn: '1d'})
+}
+
+
 
 // login
 
@@ -16,8 +25,13 @@ const signupStudent = async (req,res) => {
     const {name,email,password} = req.body;
 
     try {
+        // from the static function in the student model
         const student = await Student.signup(name,email,password);
-        res.status(200).json({name, email,student});
+
+        // create JWT web token
+        const token = createToken(student._id)
+
+        res.status(200).json({email,token});
     }
 
     catch(error) {
